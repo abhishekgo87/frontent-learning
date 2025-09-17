58e2ea4e2226b3fc78c7131b742fdbf3ef27fcc7
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-09-17 09:32:39 +0530

    m

--- js-files/3_conversion.js
+++ js-files/3_conversion.js
@@ -23,5 +23,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
  let stringNumber = String(oneNumber)
 
- console.log (stringNumber);
- console.log(typeof stringNumber);
+//  console.log (stringNumber);
+//  console.log(typeof stringNumber);
+
+
+ //         Operation
+
+ let  value = 3
+  let negValue = -value
+  //  console.log(negValue);
+ let str1 = "hello"
+ let str2 = "Abhishek"
+
+ let str3 = str1 + str2
+//  console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+ 
+
