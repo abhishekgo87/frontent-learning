be93006322f20e0912a568e1df2989c95a330a46
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-09-20 10:01:31 +0530

    3

---
+++ js-files/8_dates.js
@@ -0,0 +1,30 @@
+let myDate = new  Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("2025, 8, 18")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+// console.log(Date.now());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
+
+//`${newDATE.gETday()} and the time `
+
+newDate.toLocaleString('default', {
+   weekday: "long",
+   
+})
+
+
+
+
---
+++ js-files/9_arrays.js
@@ -0,0 +1,20 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ['shaktiman', "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[2]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+
+// console.log(myArr)
+console.log(myArr.includes(9));
+
+console.log(myArr);
+console.log(typeof newArr);
+
+
