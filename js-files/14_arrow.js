ca615008f268000177a46b6060f33360a766e10b
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-09-22 20:37:29 +0530

    05

---
+++ js-files/14_arrow.js
@@ -0,0 +1,41 @@
+const user = {
+  username: "Abhishek",
+  price: 999,
+
+  welcomeMessage: function() {
+    console.log(`${this.username} , welcome to website`);
+    console.log(this);
+  }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai() {
+//   let username = "Abhishek"
+//   console.log(this.username);
+// }
+// chai()
+
+// const chai =  function () {
+//   let username = "Abhishek"
+//   console.log(this.username);
+// }
+
+const chai =  ()  => {
+  let username = "Abhihek "
+  console.log(this);
+}
+
+// chai()
+ const addTwo = (num1, num2) => {
+   return num1 + num2
+
+}
+console.log(addTwo(3,5))
+
+const myArray = [2 ,5 ,3, 7 ,8]
+myArray.forEach(() => {})
---
+++ js-files/16_controlflow.js
@@ -0,0 +1,25 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature < 50 ){
+//   console.log("less than 50 ");
+// }
+
+
+// console.log("temperature is greater than 50");
+//  if (balance > 500) {
+
+//  }
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoggle = false
+const loggedInFromEmail = true
+
+if (isUserloggedIn && debitCard && 2==3) {
+   console.log("Allow to buy course");
+}
+
+if (loggedInFromGoggle || loggedInFromEmail) {
+    console.log("User logged in ");
+}
