e4cf0ac0e3051838b3a80994c13d56c04b072dc9
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-09-30 11:01:32 +0530

    11

---
+++ js-files/34_gettersSetters.js
@@ -0,0 +1,17 @@
+class User {
+  constructor(email, password){
+    this.email = email;
+    this.password = password
+  }
+
+  get password(){
+    return this._password.toUpperCase()
+  }
+
+  set password(value){
+    this._password = value.toUpperCase
+  }
+}
+
+const Abhishek = new User("abhi@.ai", "123")
+console.log(Abhishek.password)
