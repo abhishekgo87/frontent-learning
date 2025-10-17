613663b8e56eb0a6a268a5e1ce0256be414dbb82
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-10-17 20:43:07 +0530

    update

--- React-Redux/Redux/Counter-react-Redux/src/components/DisplayCounter.jsx
+++ React-Redux/Redux/Counter-react-Redux/src/components/DisplayCounter.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
- const counter  = useSelector(store => store.counter);
+ const counter  = useSelector((store) => store.counter);
 
   return (
     <p className="lead mb-4">
--- React-Redux/Redux/Counter-react-Redux/src/store/index.js
+++ React-Redux/Redux/Counter-react-Redux/src/store/index.js
@@ -1,14 +1,25 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
 const INITIAL_VALUE = {
-  counter: 0
-}
+  counter: 0,
+  privacy: false,
+};
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
-  console.log("Action Recived", action);
+  if (action.type === "INCREMENT") {
+    return { ...store, counter: store.counter + 1};
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1  };
+  } else if (action.type === "ADD") {
+    return { ...store,counter: store.counter + Number(action.payload.num )  };
+  } else if (action.type === "SUBTRACT") {
+    return { ...store,counter: store.counter - Number(action.payload.num) };
+  } else if (action.type === "PRIVACY_TOGGLE") {
+    return {...store, privacy: !store.privacy };
+  }
+
   return store;
-}
- const counterStore = createStore(counterReducer);
+};
+const counterStore = createStore(counterReducer);
 
-
- export default counterStore;
+export default counterStore;
